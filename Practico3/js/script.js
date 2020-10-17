a38d9cf0d20b1901a516dd4bbca14c6942db2b8a
ByElection <gonza.zarzabal@gmail.com>
2020-10-16 23:32:19 -0300

    ¿cual es tu deseo?

--- Practico3/js/script.js
+++ Practico3/js/script.js
@@ -1,21 +1,21 @@
+function countdown() {
+  let countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+  let x = setInterval(function() {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.querySelector("#countdown").innerHTML = days + "D " + hours + "H "
+    + minutes + "M " + seconds + "S ";
+    if (distance < 0) {
+      clearInterval(x);
+      document.querySelector("#countdown").innerHTML = "Ya Estreno";
+    }
+  }, 1000);
+}
 window.addEventListener('scroll', () => {
   let scroll = window.scrollY;
   document.body.style.setProperty('--scroll',scroll);
-  console.log("scroll: "+scroll)
 }, false);
-
-let countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
-let x = setInterval(function() {
-  let now = new Date().getTime();
-  let distance = countDownDate - now;
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.querySelector("#countdown").innerHTML = days + "D " + hours + "H "
-  + minutes + "M " + seconds + "S ";
-  if (distance < 0) {
-    clearInterval(x);
-    document.querySelector("#countdown").innerHTML = "Ya Estreno";
-  }
-}, 1000);
