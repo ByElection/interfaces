293b00e3f6f906a0aa94cc66933b8f0199aebdfe
ByElection <gonza.zarzabal@gmail.com>
2020-10-18 19:14:46 -0300

    ya falta poco

--- Practico3/js/script.js
+++ Practico3/js/script.js
@@ -26,20 +26,52 @@ function cargar(contenedor, contenido) {
     response.text().then(text =>{
       document.querySelector("."+contenedor).innerHTML = text;
       if (contenido != "home") {
-        dimensionarcontenido();
+        document.querySelector(".contenido").classList.remove("hide");
+        document.querySelector(".contenido").scrollIntoView(false);
       }
     }).then(function() {
       setTimeout(function() {
         fetch("../html/"+contenido+".html").then(response =>{
           response.text().then(text =>{
+            if (contenido != "home"){
+              document.querySelector(".contenido").scrollIntoView(false);
+            }
             document.querySelector("."+contenedor).innerHTML = text;
             if (contenido === "home"){
               loadhome();
+            }else {
+              animaentrada();
+              eventovolver();
             }
           });
         });
-      },5000);
+      },2000);
     });
+  });
+}
+function animaentrada() {
+  let contenido = document.querySelector(".contenido");
+  contenido.classList.add("entrada");
+  document.body.classList.add("denegarscroll");
+  setTimeout(function() {
+    contenido.classList.remove("entrada");
+  },1000);
+}
+function animasalida() {
+  let contenido = document.querySelector(".contenido");
+  contenido.classList.add("salida");
+  document.body.classList.remove("denegarscroll");
+  setTimeout(function () {
+    document.body.scrollIntoView(true);
+    contenido.classList.remove("salida");
+    document.querySelector(".contenido").classList.add("hide");
+    document.querySelector(".contenido").innerHTML = "";
+  }, 1000);
+}
+function eventovolver(){
+  document.querySelector("#volver").addEventListener("click",function(event) {
+    event.preventDefault();
+    animasalida();
   });
 }
 function eventos() {
@@ -49,39 +81,42 @@ function eventos() {
   document.querySelector("#vereventos").addEventListener("click",function() {
     cargar("contenido", "vereventos");
   });
+  document.querySelector("#spoiler").addEventListener("click",function() {
+    cargar("contenido", "spoilerzone");
+  });
   document.querySelector("#mensajegoku").addEventListener("click",function() {
     cargar("contenido", "mensajegoku");
   });
   document.querySelector("#revivirkrilin").addEventListener("click",function() {
-
+    document.querySelector("#dialogo p").innerHTML = "Krillin ya resusito demaciado";
+    recargardialogo();
   });
   document.querySelector("#crecer5cm").addEventListener("click",function() {
-
+    document.querySelector("#dialogo p").innerHTML = "Ese deseo es muy facil de hacer";
+    recargardialogo();
   });
   document.querySelector("#viajartiempo").addEventListener("click",function() {
-
+    document.querySelector("#dialogo p").innerHTML = "Solo Supermo Kaiosama tiene permitido eso";
+    recargardialogo();
   });
   document.querySelector("#inmortal").addEventListener("click",function() {
-
+    document.querySelector("#dialogo p").innerHTML = "Ese deseo es muy facil de hacer";
+    recargardialogo();
   });
   document.querySelector("#resscell").addEventListener("click",function() {
-
+    document.querySelector("#dialogo p").innerHTML = "Ese deseo es muy facil de hacer";
+    recargardialogo();
   });
 }
-function dimensionarcontenido() {
-  let contenido = document.querySelector(".contenido");
-  if (!contenido.classList.contains("dimensionescontenido")) {
-    contenido.classList.add("dimensionescontenido");
-  }
+function recargardialogo(){
+  setTimeout(function() {
+    document.querySelector("#dialogo p").innerHTML = "¿Cual es tu deseo?";
+  },3000);
 }
 function loadhome() {
   countdown();
   eventos();
   setInterval(function() {
-    if (document.querySelector("#rayos").style.display === "none") {
-      document.querySelector("#rayos").style.display = "block";
-    } else {
-      document.querySelector("#rayos").style.display = "none";
-    }
+    document.querySelector("#rayos").classList.toggle("hide");
   },500);
 }
